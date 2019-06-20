ad509a766e73f14b8bb04e13d0b33c86d315f47b
Matteo Contrini <matteo@contrini.it>
2019-06-20 22:28:15 +0200

    Update templates

--- views/emails/postMentionedHtml.blade.php
+++ views/emails/postMentionedHtml.blade.php
@@ -2,9 +2,8 @@ Ciao {!! $user->username !!}!
 <br><br>
 <i>{!! $blueprint->reply->user->username !!}</i> ha risposto a un tuo messaggio nella discussione
 <a href="{!! app()->url() !!}/d/{!! $blueprint->reply->discussion_id !!}/{!! $blueprint->reply->number !!}">
-<i>{!! $blueprint->post->discussion->title !!}</i>.
-</a>
+<i>{!! $blueprint->post->discussion->title !!}</i></a>.
 <br><br>
 --- <i>{!! $blueprint->reply->user->username !!}</i> ha scritto ---
-<br><br>
+<br>
 {!! $blueprint->reply->contentHtml !!}
--- views/emails/userMentionedHtml.blade.php
+++ views/emails/userMentionedHtml.blade.php
@@ -2,9 +2,8 @@ Ciao {!! $user->username !!}!
 <br><br>
 <i>{!! $blueprint->post->user->username !!}</i> ti ha menzionato nella discussione
 <a href="{!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}">
-<i>{!! $blueprint->post->discussion->title !!}</i>.
-</a>
+<i>{!! $blueprint->post->discussion->title !!}</i></a>.
 <br><br>
 --- <i>{!! $blueprint->post->user->username !!}</i> ha scritto ---
-<br><br>
+<br>
 {!! $blueprint->post->contentHtml !!}
